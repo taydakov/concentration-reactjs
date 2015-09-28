bb58a986acf5ff45baf7c0453a827c45d916ab0e
Lev Taydakov <lev.taydakov@gmail.com>
2015-09-28 03:35:48 -0700

    v0.0.1

--- src/javascripts/utils/History.js
+++
@@ -1,6 +0,0 @@
-import { createHistory, useBasename } from 'history';
-
-// Use history object in <Router history={ history }> to use clean URLs
-const history = useBasename(createHistory)({
-  basename: ''
-})
--- src/javascripts/utils/Routines.js
+++
@@ -1,7 +0,0 @@
-// exports.fill = function (length = 0, value = false) {
-// 	var array = new Array(length);
-// 	for (var i = 0; i < length; i++) {
-// 		array[i] = value;
-// 	}
-// 	return array;
-// }
