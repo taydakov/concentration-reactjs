69c6027203530a3c5e3165d90811b62a69645da6
Lev Taydakov <lev.taydakov@gmail.com>
2015-09-28 03:46:15 -0700

    Refactor

--- src/javascripts/components/ConcentrationGameApp.js
+++ src/javascripts/components/ConcentrationGameApp.js
@@ -5,24 +5,8 @@ import React from 'react';
 import { Link } from 'react-router';
 /** React Components **/
 import MainTitle from './MainTitle';
-// import ChooseLevel from './ChooseLevel';
-/** Stores **/
-// import FiltersStore from '../stores/FiltersStore';
-/** Action creators **/
-// import FiltersActionCreators from '../actions/FiltersActionCreators';
-
-function getStoresState (ConcentrationGameAppObj) {
-	return {
-		// filters: FiltersStore.getState()
-	};
-}
 
 export default class ConcentrationGameApp extends React.Component {
-	constructor (...args) {
-		super(...args);
-
-		this.state = getStoresState(this);
-	}
 
 	render () {
 		var self = this;
@@ -35,14 +19,4 @@ export default class ConcentrationGameApp extends React.Component {
 		);
 	}
 
-	/* Event handlers */
-
-	handleFilterClicked (selectedFilters) {
-		console.log('ConcentrationGameApp.handleFilterClicked selectedFilters=', selectedFilters);
-	}
-
-	_onChange () {
-		var newState = getStoresState(this);
-		this.setState(newState);
-	}
 };
